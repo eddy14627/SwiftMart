038d9c78b05802f4eac1d57f7adc2476310a258b
adarsh <adjee2020@gmail.com>
2023-02-04 14:03:52 +0530

    created redux store

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -31,7 +31,9 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "product not found" });
+      // res.status(404).json({ message: "product not found" }); or
+      res.status(404);
+      throw new Error("product not found");
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -22,6 +23,9 @@ app.use("/api/products", productRoutes);
 //   const d = products.find((p) => p._id === req.params.id);
 //   res.json(d);
 // });
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV;
